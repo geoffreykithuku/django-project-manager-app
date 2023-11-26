f5e0211c967da96cdcb4ea5bcf15b4a458c7745d
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-11-26 21:31:03 +0300

    fix image

--- taskmanager/settings.py
+++ taskmanager/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
